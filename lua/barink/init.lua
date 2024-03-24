a1ef89b3c9f2fa7c9e1a79f23dd2d0e9cd15b3da
Nigel Barink <nigelbarink@hotmail.com>
2024-03-24 22:45:52 +0100

    Added LSP and cmp

--- lua/barink/init.lua
+++ lua/barink/init.lua
@@ -1,73 +1,149 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-	{
-	  "folke/tokyonight.nvim",
-	  lazy = false,
-	  priority = 1000,
-	  opts = {},
-      config = function ()
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
             require("tokyonight").setup({
-            	style="night",
-            	light_style="day",
-            	transparent=true,
-            	terminal_colors=true,
-            	dim_inactive=false,
-            	lualine_bold=false,
+                style = "night",
+                light_style = "day",
+                transparent = true,
+                terminal_colors = true,
+                dim_inactive = false,
+                lualine_bold = false,
             })
-            -- make transparent BG
---            vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
---            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 
-
-            vim.cmd[[colorscheme tokyonight]]
+            vim.cmd [[colorscheme tokyonight]]
         end
-	},
-	{
-		"nvim-telescope/telescope.nvim", 
-		dependencies = { 'nvim-lua/plenary.nvim'}
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function ()
-			local configs = require("nvim-treesitter.configs")
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
-				ensure_installed = {"c", "lua", "vim", "vimdoc", "bash"},
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-			end
-	},
-	{"mbbill/undotree"},
-	{ 'VonHeikemen/lsp-zero.nvim', branch='v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
-	{'tpope/vim-fugitive'},
+            configs.setup({
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "bash" },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    },
+    { "mbbill/undotree" },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            "williamboman/mason-lspconfig",
+            "williamboman/mason.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
+            'L3MON4D3/LuaSnip',
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "bashls", "rust_analyzer" }
+            })
+           local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+
+            -- Set configuration for specific filetype.
+            cmp.setup.filetype('gitcommit', {
+                sources = cmp.config.sources({
+                    { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+
+            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+            cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+
+            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    { name = 'cmdline' }
+                }),
+                matching = { disallow_symbol_nonprefix_matching = false }
+            })
+
+            -- Set up lspconfig.
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            require('lspconfig')['asm_lsp'].setup({
+                capabilities = capabilities
+            })
+
+            local lsp = require("lspconfig")
+            lsp.lua_ls.setup({
+                capabilities = require('cmp_nvim_lsp').default_capabilities(),
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { 'vim' }
+                        }
+                    }
+                }
+            })
+        end
+    },
+    { 'tpope/vim-fugitive' },
+    {
+        "j-hui/fidget.nvim",
+        config = function()
+            require('fidget').setup({})
+        end
+    }
 
 }, {})
-
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({buffer = bufnr})
-end)
 
 -- Set basic vim options
 vim.opt.guicursor = ""
@@ -92,8 +168,8 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 
--- Keymaps 
-vim.g.mapleader = " " 
+-- Keymaps
+vim.g.mapleader = " "
 
 local builtin = require('telescope.builtin')
 
@@ -104,13 +180,12 @@ vim.keymap.set('n', '<leader><F8>', vim.cmd.UndotreeToggle)
 
 vim.keymap.set('n', '<leader>p', [["_dP]])
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
-
-osname = vim.loop.os_uname().sysname
-if osname == "Windows_NT" then 
+vim.keymap.set('n', '<leader>m', ":Mason<CR>")
+local osname = vim.loop.os_uname().sysname
+if osname == "Windows_NT" then
     print("Windows Detected!!")
-    vim.keymap.set('n', '<leader>x', function() print("sorry this is windows!")end)
-elseif osname == "Linux"  then 
+    vim.keymap.set('n', '<leader>x', function() print("sorry this is windows!") end)
+elseif osname == "Linux" then
     -- Linux only!!
-    vim.keymap.set('n', '<leader>x', "<cmd>!chmod +x %<CR>", { silent =true })
+    vim.keymap.set('n', '<leader>x', "<cmd>!chmod +x %<CR>", { silent = true })
 end
-
