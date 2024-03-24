0bb6067bb946c2b1b7555cdc1acb20669b566da7
Nigel Barink <nigelbarink@hotmail.com>
2024-03-24 17:17:11 +0100

    Added additional mappings

--- lua/barink/init.lua
+++ lua/barink/init.lua
@@ -102,3 +102,15 @@ vim.keymap.set('n', '<leader>fs', builtin.git_files, {})
 
 vim.keymap.set('n', '<leader><F8>', vim.cmd.UndotreeToggle)
 
+vim.keymap.set('n', '<leader>p', [["_dP]])
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+
+osname = vim.loop.os_uname().sysname
+if osname == "Windows_NT" then 
+    print("Windows Detected!!")
+    vim.keymap.set('n', '<leader>x', function() print("sorry this is windows!")end)
+elseif osname == "Linux"  then 
+    -- Linux only!!
+    vim.keymap.set('n', '<leader>x', "<cmd>!chmod +x %<CR>", { silent =true })
+end
+
