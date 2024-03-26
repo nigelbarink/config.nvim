ab7b92e17963493acd7bc90785357047425964db
Nigel Barink <nigelbarink@hotmail.com>
2024-03-26 12:57:57 +0100

    Removed trailing empty lines and remove Hello windows debug print

--- lua/barink/keys.lua
+++ lua/barink/keys.lua
@@ -7,7 +7,6 @@ vim.keymap.set('n', '<leader>fa', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>m', ":Mason<CR>")
 local osname = vim.loop.os_uname().sysname
 if osname == "Windows_NT" then
-    print("Windows Detected!!")
     vim.keymap.set('n', '<leader>x', function() print("sorry this is windows!") end)
 elseif osname == "Linux" then
     -- Linux only!!
