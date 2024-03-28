0600421787db7ea6ff16f96403359c59b558614d
Nigel Barink <nigelbarink@hotmail.com>
2024-03-28 19:34:49 +0100

    FEAT: Added fzf to telescope

--- lua/barink/plugins/telescope.lua
+++ lua/barink/plugins/telescope.lua
@@ -1,10 +1,25 @@
+local os = vim.loop.os_uname().sysname
 return {
         {
             "nvim-telescope/telescope.nvim",
             dependencies = {
-                'nvim-lua/plenary.nvim',
+                {'nvim-lua/plenary.nvim'},
+                {
+                    'nvim-telescope/telescope-fzf-native.nvim',
+                    build = function () if os == 'Windows_NT' then
+                        print("You are on Windows, Idiot!!!")
+                       return [[
+                                cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release
+                                && cmake --build build --config Release 
+                                && cmake --install build --prefix build
+                                ]]  else
+                        return 'make' end
+                    end,
+                }
             },
             config = function()
+                require('telescope').setup({})
+                require('telescope').load_extension('fzf')
                 local builtin = require('telescope.builtin')
 
                 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
